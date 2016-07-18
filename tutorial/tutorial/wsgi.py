9d147dd7f46fc42124b8e75e4b667510d0da03e1
chris-r-anderson12 <chris.r.anderson12@gmail.com>
2016-07-17 17:20:18 -0700

    First commit
    First commit

---
+++ tutorial/tutorial/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for BioMap project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BioMap.settings")
+
+application = get_wsgi_application()
